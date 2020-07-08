8827f55711096505bf5ce012d9a03422cf01191d
jam-e-dev <jam.e.dev.mail@gmail.com>
2020-07-08 22:22:55 +0100

    OS-14 - Added patient telecom table creator.

---
+++ Osmosys/Server/Database/Tables/Patients/Telecoms/IPatientTelecomTableCreator.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Server.Database.Tables.Patients.Telecoms
+{
+    public interface IPatientTelecomTableCreator
+    {
+        Task CreateIfNotExistsAsync();
+    }
+}
---
+++ Osmosys/Server/Database/Tables/Patients/Telecoms/PatientTelecomTableCreator.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Npgsql;
+using Server.Database.Connection;
+
+namespace Server.Database.Tables.Patients.Telecoms
+{
+    public class PatientTelecomTableCreator : IPatientTelecomTableCreator
+    {
+        private readonly DbConnection _connection;
+
+        public PatientTelecomTableCreator(DbConnection connection)
+        {
+            _connection = connection;
+        }
+        
+        public async Task CreateIfNotExistsAsync()
+        {
+            const string sql = "create table if not exists patient_telecoms (pk bigserial primary key, patient_fk bigint, foreign key patient_fk references patients(pk), system text, value text, use text, rank int, period_start timestamp, period_end timestamp)";
+            await using var cmd = new NpgsqlCommand(sql, _connection.Current);
+            await cmd.ExecuteNonQueryAsync();
+        }
+    }
+}
