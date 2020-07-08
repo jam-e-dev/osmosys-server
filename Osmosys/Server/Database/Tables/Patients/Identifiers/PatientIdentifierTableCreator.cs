5d21a22e7a656337498903341ee528441b07df73
jam-e-dev <jam.e.dev.mail@gmail.com>
2020-07-08 22:06:40 +0100

    OS-13 - Added missing implementation.

--- Osmosys/Server/Database/Tables/Patients/Identifiers/PatientIdentifierTableCreator.cs
+++ Osmosys/Server/Database/Tables/Patients/Identifiers/PatientIdentifierTableCreator.cs
@@ -15,7 +15,7 @@ namespace Server.Database.Tables.Patients.Identifiers
         
         public async Task CreateIfNotExistsAsync()
         {
-            const string sql = "create table if not exists patient_identifiers (pk bigserial primary key, patient_fk bigint, foreign key (patient_fk) references patients(pk), use text, identifier_type_fk bigint, foreign key (identifier_type_fk) references identifier_types(pk), system text, value text, period_start timestamp, period_end timestamp)";
+            const string sql = "create table if not exists patient_identifiers (pk bigserial primary key, patient_fk bigint, foreign key (patient_fk) references patients(pk), identifier_type_fk bigint, foreign key (identifier_type_fk) references identifier_types(pk), use text, system text, value text, period_start timestamp, period_end timestamp)";
             await using var cmd = new NpgsqlCommand(sql, _connection.Current);
             await cmd.ExecuteNonQueryAsync();
         }
