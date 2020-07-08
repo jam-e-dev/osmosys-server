59d256bf9da863c164d7a6677ddf1887b8f50fe7
jam-e-dev <jam.e.dev.mail@gmail.com>
2020-07-08 23:13:43 +0100

    OS-17 - Added patient contact relationship tables.

---
+++ Osmosys/Server/Database/Tables/Patients/Contacts/Relationships/IPatientContactRelationshipTableCreator.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Server.Database.Tables.Patients.Contacts.Relationships
+{
+    public interface IPatientContactRelationshipTableCreator
+    {
+        Task CreateIfNotExistsAsync();
+    }
+}
