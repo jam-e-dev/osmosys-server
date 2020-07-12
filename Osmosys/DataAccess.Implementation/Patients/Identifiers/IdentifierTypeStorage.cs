fc14aff0466bd2d8c17bebb14f688afc13ed56e5
jam-e-dev <jam.e.dev.mail@gmail.com>
2020-07-12 21:40:05 +0100

    OS-16 - Cleanup of identifier type data init.

--- Osmosys/DataAccess.Implementation/Patients/Identifiers/IdentifierTypeStorage.cs
+++ Osmosys/DataAccess.Implementation/Patients/Identifiers/IdentifierTypeStorage.cs
@@ -7,19 +7,47 @@ namespace DataAccess.Implementation.Patients.Identifiers
     {
         private readonly IIdentifierTypeTableCreator _identifierTypeTableCreator;
         private readonly IIdentifierTypeCodingTableCreator _identifierTypeCodingTableCreator;
+        private readonly IIdentifierTypeRecordReader _identifierTypeRecordReader;
+        private readonly IIdentifierTypeRecordWriter _identifierTypeRecordWriter;
 
         public IdentifierTypeStorage(
             IIdentifierTypeTableCreator identifierTypeTableCreator,
-            IIdentifierTypeCodingTableCreator identifierTypeCodingTableCreator)
+            IIdentifierTypeCodingTableCreator identifierTypeCodingTableCreator,
+            IIdentifierTypeRecordReader identifierTypeRecordReader,
+            IIdentifierTypeRecordWriter identifierTypeRecordWriter)
         {
             _identifierTypeTableCreator = identifierTypeTableCreator;
             _identifierTypeCodingTableCreator = identifierTypeCodingTableCreator;
+            _identifierTypeRecordReader = identifierTypeRecordReader;
+            _identifierTypeRecordWriter = identifierTypeRecordWriter;
         }
         
         public async Task InitAsync()
         {
+            await InitTablesAsync();
+            await InitDataAsync();
+        }
+
+        private async Task InitTablesAsync()
+        {
             await _identifierTypeTableCreator.CreateIfNotExistsAsync();
             await _identifierTypeCodingTableCreator.CreateIfNotExistsAsync();
+        }
+
+        private async Task InitDataAsync()
+        {
+            var identifierTypeCount = await _identifierTypeRecordReader.CountAsync();
+            var noIdentifierTypesFound = identifierTypeCount < 1;
+            
+            if (noIdentifierTypesFound)
+            {
+                var defaultTypes = IdentifierTypeDefaults.Defaults;
+
+                foreach (var type in defaultTypes)
+                {
+                    await _identifierTypeRecordWriter.WriteAsync(type);
+                }
+            }
         }
     }
 }
